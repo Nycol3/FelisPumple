66c9a68316ce6d37deb69a206d14e19973f95971
Nycol3 <nicolesoy123@outlook.com>
2025-10-16 19:24:01 -0300

    Subida

---
+++ script.js
@@ -0,0 +1,96 @@
+
+/*Movimientos de la carta*/
+const envoltura = document.querySelector(".envoltura-sobre");
+const carta = document.querySelector(".carta");
+
+document.addEventListener("click", (e) => {
+    if (e.target.matches(".sobre") || 
+        e.target.matches(".solapa-derecha") ||
+        e.target.matches(".solapa-izquierda") ||
+        e.target.matches(".corazon")) {
+        envoltura.classList.toggle("abierto");
+        envoltura.classList.add("desactivar-sobre")
+
+        if (!carta.classList.contains("abierta")) {
+            setTimeout(() => {
+                carta.classList.add("mostrar-carta");
+
+                setTimeout(() => {
+                    carta.classList.remove("mostrar-carta");
+                    carta.classList.add("abierta");
+                }, 500);
+            }, 1000);
+        }
+    } else if (e.target.matches(".envoltura-sobre *")) {
+        envoltura.classList.remove("abierto");
+        envoltura.classList.remove("desactivar-sobre")
+        if (carta.classList.contains("abierta")) {
+            carta.classList.add("cerrando-carta");
+
+            setTimeout(() => {
+                carta.classList.remove("cerrando-carta");
+                carta.classList.remove("abierta")
+            }, 500);
+        }
+    }
+})
+
+/*Movimietnos de globos*/
+const balloonContainer = document.getElementById("contenedor-globos");
+
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function getRandomStyles() {
+  var r = random(255);
+  var g = random(255);
+  var b = random(255);
+  var mt = random(200);
+  var ml = random(50);
+  var dur = random(5) + 5;
+  return `
+  background-color: rgba(${r},${g},${b},0.7);
+  color: rgba(${r},${g},${b},0.7); 
+  box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+  margin: ${mt}px 0 0 ${ml}px;
+  animation: float ${dur}s ease-in infinite
+  `;
+}
+
+function createBalloons(num) {
+  for (var i = num; i > 0; i--) {
+    var balloon = document.createElement("div");
+    balloon.className = "balloon";
+    balloon.style.cssText = getRandomStyles();
+    balloonContainer.append(balloon);
+  }
+}
+
+function removeBalloons() {
+  balloonContainer.style.opacity = 0;
+  setTimeout(() => {
+    balloonContainer.remove()
+  }, 500)
+}
+
+window.addEventListener("load", () => {
+  createBalloons(30)
+});
+
+window.addEventListener("load", function() {
+    alert("Advertencia: Tenele paciencia a la carta. Es media boba y se tilda la muy puta si la apretas muchas veces seguidas.");
+    this.alert("Bueno, nada, eso :)");
+});
+
+/*Boton*/
+function mostrarBoton() {
+    const botonEmoji = document.querySelector(".emoji-globo");
+    const boton = document.querySelector(".boton-raro");
+
+    botonEmoji.addEventListener("click", (e) => {
+        boton.style.display = "block";
+    }
+);   
+}
+mostrarBoton();
